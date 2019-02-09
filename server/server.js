748d19d5ba3b1e812dfd09afc9543f82cff50007
JasonGITHUB <Jasonvirtualai@gmail.com>
2019-02-09 13:33:19 -0800

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -4,23 +4,33 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000; // heroku------
+const port = process.env.PORT || 3000; // --------heroku------
 
-var app = express();
+var app = express(); // start up server
 var server = http.createServer(app);
 var io = socketIO(server);
 
 
 app.use(express.static(publicPath)); // middleware------
 
-io.sockets.on('connection', function (socket) {
-    console.log('A client is connected!');
-});
+// io.sockets.on('connection', function (socket) { // reqular function
+//     console.log('A client is connected!');
+// });
 
-io.on('connection', (socket)  => {
+io.on('connection', (socket)  => {   //ES6
   console.log('New user connected');
 
-   socket.on('disconnect', () => {
+  socket.emit('newMessage', {
+     from: 'Jason ',
+     text: 'See you then.',
+     createAt: 123123
+   });
+
+   socket.on('createMessage', (message) => {
+     console.log('creatMessage', message);
+   })
+
+   socket.on('disconnect', (socket) => {
     console.log('User was disconnected');
   });
 });
